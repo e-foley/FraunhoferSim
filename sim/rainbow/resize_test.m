f73846f38163f6528fed799415b29054aca5113d
Ed Foley <efoley@calpoly.edu>
2017-08-05 15:31:39 -0700

    consolidate resize tests

--- sim/rainbow/resize_test.m
+++ sim/rainbow/resize_test.m
@@ -1,58 +1,37 @@
 clear all;
 
+%img = imread('dice.png');
 %img = imread('cat.png');
-%img = imread('mario.png');
+%img = imread('mario.png');  % use center [0.27 0.415]
 %img = imread('peacock.jpg');
 %img = imread('donkey.png');
-%img = imread('bridged2.png');
-img = imread('target.png');
-img = rgb2gray(img);
+%img = imread('bridged3.png');
+%img = imread('target.png');
+%img = imread('statue.jpg');  % center [0.42 0.35]
+%img = imread('piano.jpeg');
+%img = imread('citysmall.png');
+%img = imread('city2.jpg');  % center [0.3 0.28]
+%img = imread('bowtie simple psf.png');
+img = imread('bridged3.png');
 
-steps = 3;
-
-lambda_low = 380;
-lambda_high = 780;
-wavelengths = linspace(lambda_low, lambda_high, steps);
-gamma = 0.8;
+mono = false;
+steps = 20;
+lambda_low = 380;  % 380 to 670 range gives whites a little yellowy, but not bad
+lambda_high = 660;  % up to 640 improves whites but makes image look green
+gamma = 0.8;  % 0.8 is default
+border_shade = 0.0;
+brightness_cut = -0.5;  % larger -> brighter
+scale_factor = 1.0;
 output_size = [size(img, 1) size(img, 2)];
+center = [0.5 0.5];
+scales = linspace(1.0 / (1.0 + scale_factor*(lambda_high/lambda_low - 1.0)), 1.0, steps);  % scale factor implementation may not be correct...
+%scales = linspace(1.0, 1.0 + scale_factor*(lambda_high/lambda_low - 1.0), steps);
+wavelengths = linspace(lambda_low, lambda_high, steps);
+brightnesses = (steps ^ brightness_cut) .* ones(1, steps);
 
+if mono
+    img = rgb2gray(img);
+end
 
-scale_range = [lambda_high/lambda_low 1.0];
-%hue_range = [0 1-1/steps];
-
-i_max = numel(wavelengths);
-% for i=1:i_max
-%     hsv = rgb2hsv(lambda2rgb(wavelengths(i), gamma));
-%     hue_range(i) = rgb2
-
-%hue_range = [0 0.8];
-border_shade = 1.0;
-brightness = steps ^ -0.75;  % Seems to be a good ratio
-%brightess = 1;
-%brightness = 0.25;
-
-%tint_color = [1.0 1.0 0.0];
-
-% tint(img, tint_color);
-% imshow(img_out);
-
-% img_red   = tint(scale_dimkeep(img, 1.0, output_size, 0.0), [1.0 0.0 0.0]);
-% img_green = tint(scale_dimkeep(img, 1.0, output_size, 0.0), [0.0 1.0 0.0]);
-% img_blue  = tint(scale_dimkeep(img, 1.0, output_size, 0.0), [0.0 0.0 1.0]);
-
-%img_res = scale_dimkeep(img, 1.0, output_size, 0.5);
-
-
-
-% imshow(img_res);
-
-% imshow(img_red + img_green + img_blue);
-
-
-% scales = linspace(1.0, 1.2, steps);
-% hues = linspace(0.0, 
-
-%tester = rainbowify(img, scale_range, hue_range, steps, output_size, border_shade, brightness);
-tester = rainbowify_lambdas(img, scale_range, wavelengths, steps, output_size, border_shade, brightness);
-
+tester = rainbowify_lambdas(img, scales, wavelengths, brightnesses, gamma, border_shade, output_size, center);
 imshow(tester);
