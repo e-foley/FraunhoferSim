d3620957d242e8eb88f1dcddbaf6744021061748
e-foley <efoley@calpoly.edu>
2019-05-13 22:30:30 -0700

    clarify psf ld bounds field

--- sim/Psf.m
+++ sim/Psf.m
@@ -1,7 +1,7 @@
 classdef Psf
     properties
         data  % the actual power spectrum magnitudes
-        pixels_per_ld  % pixels/(lambda/diameter)
-        ld_bounds  % dimensions of image in lambda/diameter [vert; horiz]
+        pixels_per_ld  % pixels/(lambda/D)
+        ld_bounds  % limits of image in lambda/D [v1 v2; u1 u2]
     end
 end
