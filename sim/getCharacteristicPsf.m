e8879e0b2489522e2ae9fc0a2bae5c5a049a662a
e-foley <efoley@calpoly.edu>
2019-05-13 21:47:50 -0700

    define getcharacteristicpdf
    It's based heavily on getCleverPowerSpectrum but doesn't do convolution and uses Psf object.

---
+++ sim/getCharacteristicPsf.m
@@ -0,0 +1,28 @@
+function [psf, reduced_input_size, fft_size] = ...
+    getCharacteristicPsf(aperture, input_scale, fft_scale)
+
+psf = Psf;
+
+% Reduce dims of the mask/aperture so FFT uses less memory after padding.
+reduced_aperture = imresize(aperture, input_scale);
+reduced_input_size = size(reduced_aperture);
+fft_size = fft_scale * reduced_input_size;
+
+% Find FFT of this mask/aperture (not power spectrum yet), padding the FFT
+% to dimensions of fft_size and placing zero-frequency component in the
+% center of the image.
+psf.data = fftshift(fft2(reduced_aperture, fft_size(1), fft_size(2)));
+
+% Power spectrum is the square of the complex amplitude.
+psf.data = abs(psf.data) .^ 2;
+
+% With unity FFT scale, 1.0 lambda/D is lowest resolvable frequency; larger
+% FFT scales give better resolution.
+psf.pixels_per_ld = fft_scale;
+
+% Calculating bounds is slightly tricky because fftshift places
+% zero-frequency to lower-right of center when dimension is even.
+psf.ld_bounds(:,2) = floor((fft_size' - 1) / 2);
+psf.ld_bounds(:,1) = psf.ld_bounds(:,2) - fft_size' + 1;
+
+end
