ab9a69aa2c81ffd072060ad54de2aa25023903d2
e-foley <efoley@calpoly.edu>
2019-05-19 23:31:02 -0700

    much more effecient custom convolve

--- sim/psfConvolve.m
+++ sim/psfConvolve.m
@@ -8,30 +8,27 @@ convolved_psf.data = zeros(size(convolved_psf.data));
 x_lim = size(convolved_psf.data, 1);
 y_lim = size(convolved_psf.data, 2);
 
+% Compose the image in different slices--one slice per convolution member.
 for i = 1:size(ld_elements, 1)
     x_shift = -round(ld_elements(i, 2) * psf.pixels_per_ld);
     y_shift =  round(ld_elements(i, 1) * psf.pixels_per_ld);
+    slice = psf.data;
     
-    for x = 1:size(psf.data, 1)
-        if x + x_shift < 1 || x + x_shift > x_lim
-            continue;
-        end
-        for y = 1:size(psf.data, 2)
-            if (y + y_shift < 1 || y + y_shift > y_lim)
-                continue;
-            end
-            convolved_psf.data(x + x_shift, y + y_shift) = ...
-                convolved_psf.data(x + x_shift, y + y_shift) + ...
-                ld_elements(i, 3) * psf.data(x, y);
-        end
+    if (x_shift < 0)
+        slice = slice(-x_shift+1:end, :);
+        slice = padarray(slice, [-x_shift 0], 'post');
+    else
+        slice = slice(1:end-x_shift, :);
+        slice = padarray(slice, [x_shift 0], 'pre');
     end
-end
-
-% convolution_matrix(:,1) = ...
-%     1 + round((ld_elements(:,1) - psf.ld_bounds(1)) * psf.pixels_per_ld); 
-% convolution_matrix(:,2) = ...
-%     1 + round((ld_elements(:,2) - psf.ld_bounds(2)) * psf.pixels_per_ld);
-% 
-% convolved_psf.data = conv2(psf.data, convolution_matrix, 'same');
-
+    
+    if (y_shift < 0)
+        slice = slice(:,-y_shift+1:end);
+        slice = padarray(slice, [0 -y_shift], 'post');
+    else
+        slice = slice(:, 1:end-y_shift);
+        slice = padarray(slice, [0 y_shift], 'pre');
+    end
+    
+    convolved_psf.data = convolved_psf.data + ld_elements(i, 3) * slice;
 end
