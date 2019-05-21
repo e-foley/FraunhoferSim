b0319cab0c1486060ac3f8b0b2ec85e99d15deaf
e-foley <efoley@calpoly.edu>
2019-05-20 19:30:57 -0700

    allow optional normalization value argument

--- sim/psfGetImage.m
+++ sim/psfGetImage.m
@@ -1,7 +1,12 @@
-function [image] = psfGetImage(psf, log_10_mag_limits)
+function [image] = psfGetImage(psf, log_10_mag_limits, normalization_value)
 
-% Log-normalize the power spectrum.
-image = log10(psf.data ./ max(max(psf.data)));
+% Log-normalize the power spectrum. Use the maximum value if a
+% normalization value is not provided.
+if nargin < 3
+    normalization_value = max(max(psf.data));
+end
+
+image = log10(psf.data ./ normalization_value);
 
 % Map the log-10 magnitude limits to [0, 1]. No clamping is applied.
 mag_delta = log_10_mag_limits(2) - log_10_mag_limits(1);
