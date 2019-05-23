e82e55e42a3e66927b4d365682e10666d2cb2a73
e-foley <efoley@calpoly.edu>
2019-05-22 19:08:37 -0700

    add spectralcanvas struct
    I suspect we'll modify this more later...

---
+++ sim/SpectralCanvas.m
@@ -0,0 +1,8 @@
+classdef SpectralCanvas
+    properties
+        data  % the color data: dims 1, 2, 3 are v, u, and RGB
+        pixels_per_as  % pixels/arcsecond
+        as_bounds  % limits of image in arcseconds [v1 v2; u1 u2]
+        diameter_in  % diameter of aperture in inches
+    end
+end
