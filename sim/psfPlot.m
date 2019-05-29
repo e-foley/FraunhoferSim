8795d0049e637553fe188edde62dda8713e8b221
e-foley <efoley@calpoly.edu>
2019-05-29 00:11:21 -0700

    first functioning array psf plot

--- sim/psfPlot.m
+++ sim/psfPlot.m
@@ -13,18 +13,24 @@ composite = zeros([max_size_px 3]);
 for i=1:num_psfs
     map = s.color_map;  % TODO: INDEX
     num_colors = size(map, 1);
+    % Resize small images to size of largest.
     psf_images{i} = imresize(psf_images{i}, max_size_px);
+    % Convert psf_images to indices within color map.
     psf_images{i} = round(1 + (num_colors - 1) * max(0, min(1, psf_images{i})));
     for x=1:size(psf_images{i}, 1)
         for y=1:size(psf_images{i}, 2)
-            composite(x,y,:) = composite(x,y,:) + map(psf_images{i}(x,y,:));
+            % Add colors element-wise (3D indices are challenging in MATLAB).
+            for c=1:3
+                composite(x,y,c) = composite(x,y,c) + map(psf_images{i}(x,y),c);
+            end
         end
     end
 end
 
 figure_out = figure;
-imshow(composite);
+%imshow(composite);
 %imagesc(psf.ld_bounds(1,:), fliplr(psf.ld_bounds(2,:)), image);
+imagesc(s.field_limits(1,:), fliplr(s.field_limits(2,:)), composite);
 formatImagescPlot(figure_out, s);
 caxis(s.output_limits);
 h = colorbar;
