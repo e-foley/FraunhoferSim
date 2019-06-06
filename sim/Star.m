5a1052218f4eb9099bbda16509cd9f806812f6c9
e-foley <efoley@calpoly.edu>
2019-06-05 20:59:26 -0700

    let star hold temperature

--- sim/Star.m
+++ sim/Star.m
@@ -2,12 +2,18 @@ classdef Star
     properties
         pos_as  % "absolute" position of the star in arcseconds [u, v]
         app_vis_mag  % apparent visual magnitude of star (NOT log-10)
+        temperature_k  % temperature of star in kelvins
     end
     
     methods
-        function star = Star(pos_as, app_vis_mag)
+        function star = Star(pos_as, app_vis_mag, temperature_k)
             star.pos_as = pos_as;
             star.app_vis_mag = app_vis_mag;
+            if (nargin >= 3)
+                star.temperature_k = temperature_k;
+            else
+                star.temperature_k = 7000;  % default value is white-ish star
+            end
         end
     end
 end
--- sim/asterismFromDouble.m
+++ sim/asterismFromDouble.m
@@ -1,12 +1,23 @@
-function [asterism] = asterismFromDouble(separation_as, app_vis_mags, pa_deg)
+function [asterism] = asterismFromDouble(separation_as, app_vis_mags, pa_deg,...
+    temperature1_k, temperature2_k)
 
 % separation_as = separation [arcseconds]
 % app_vis_mags = apparent visual magnitudes
 % pa_deg = position angle of second star relative to first star [deg]
+% temperature1_k = temperature of star 1 (assumed to be a black body)
+% temperature2_k = temperature of star 2 (assumed to be a black body)
 
-% Assume primary star is centered.
-star1 = Star([0 0], app_vis_mags(1));
-star2 = Star(separation_as * [cosd(pa_deg-90) sind(pa_deg-90)], app_vis_mags(2));
+% We ssume primary star is centered.
+
+% We use simplified constructors in case temperatures are not provided.
+if nargin >= 5
+    star1 = Star([0 0], app_vis_mags(1), temperature1_k);
+    star2 = Star(separation_as * [cosd(pa_deg-90) sind(pa_deg-90)], ...
+        app_vis_mags(2), temperature2_k);
+else
+    star1 = Star([0 0], app_vis_mags(1));
+    star2 = Star(separation_as * [cosd(pa_deg-90) sind(pa_deg-90)], app_vis_mags(2));
+end
 
 asterism = [star1 star2];
 
