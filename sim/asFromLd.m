22836866a3dd8ae7c57540462fe5fa2f6b817419
e-foley <efoley@calpoly.edu>
2019-06-06 23:41:17 -0700

    mm asFromLd comment update

--- sim/asFromLd.m
+++ sim/asFromLd.m
@@ -1,5 +1,5 @@
-% Calculates the factor that converts lambda/diameter into arcseconds for
-% the given wavelength and aperture diameter.
+% Calculates the factor that converts lambda/diameter into arcseconds for the
+% given wavelength and aperture diameter.
 %
 % wavelength_nm  The wavelength of light [nanometers]
 % diameter_in    The aperture diameter [inches]
@@ -8,7 +8,7 @@
 
 function [as] = asFromLd(wavelength_nm, diameter_in)
     % Convert both arguments to meters to find angle in radians. (Small
-    % angle approximation is assumed.)
+    % angle approximation is used.)
     ld_rad = (wavelength_nm / 1e9) / (diameter_in * (1/12) * (1/3.28));
     
     % Convert angle from radians to arcseconds.
