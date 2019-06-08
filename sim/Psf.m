a3e23fb27e0a2f2b0da7ecb698054ffabbaef2ed
e-foley <efoley@calpoly.edu>
2019-06-07 21:17:34 -0700

    document Psf

--- sim/Psf.m
+++ sim/Psf.m
@@ -1,7 +1,20 @@
+% Represents a point spread function. Contains information about the 
+% intensity of the electromagnetic field at discrete angular positions (which
+% correspond to known ratios of light wavelength to aperture diameter).
+
 classdef Psf
     properties
-        data  % the actual power spectrum magnitudes
-        pixels_per_ld  % pixels/(lambda/D)
-        ld_bounds  % limits of image in lambda/D [u_min u_max; v_min v_max]
+        % Matrix with values proportional to the intensity (square of complex 
+        % amplitude) of the electromagnetic field at discrete angles. First
+        % index is values of u spanning ld_bounds(1,:); second index is values
+        % of v spanning ld_bounds(2,:).
+        data
+        
+        % Number of pixels per ratio of light wavelength to aperture diameter
+        % (pixels/(L/D))
+        pixels_per_ld
+        
+        % The angular domain of the PSF data (L/D) [umin,umax;vmin,vmax]
+        ld_bounds
     end
 end
