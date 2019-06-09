063185de550838e0e1dda93cb949076a76ba38a2
e-foley <efoley@calpoly.edu>
2019-06-09 14:49:50 -0700

    document psfGetImage

--- sim/psfGetImage.m
+++ sim/psfGetImage.m
@@ -1,3 +1,14 @@
+% Generates a graphical representation of a point spread function encoded by a
+% Psf object across a specified angle domain.
+%
+% psf                The Psf object representing the PSF to visualize
+% new_ld_bounds      The angular bounds of the image in wavelengths per aperture
+%                    diameter [umin,umax;vmin,vmax]
+% log_10_mag_limits  Log-10 magnitude limits corresponding to black and white
+%                    with intermediate values in grayscale [min,max]
+%
+% image              The generated PSF image as a matrix of grayscale values
+
 function [image] = psfGetImage(psf, new_ld_bounds, log_10_mag_limits)
 
 % Log-normalize the power spectrum.
@@ -8,7 +19,7 @@ upx_min = 1 + floor(psf.pixels_per_ld * (new_ld_bounds(1,1) - psf.ld_bounds(1,1)
 upx_max = 1 +  ceil(psf.pixels_per_ld * (new_ld_bounds(1,2) - psf.ld_bounds(1,1)));
 vpx_min = 1 + floor(psf.pixels_per_ld * (new_ld_bounds(2,1) - psf.ld_bounds(2,1)));
 vpx_max = 1 +  ceil(psf.pixels_per_ld * (new_ld_bounds(2,2) - psf.ld_bounds(2,1)));
-image = image(upx_min:upx_max, vpx_min:vpx_max);
+image = image(upx_min:upx_max,vpx_min:vpx_max);
 
 % Map the log-10 magnitude limits to [0, 1]. No clamping is applied.
 mag_delta = log_10_mag_limits(2) - log_10_mag_limits(1);
