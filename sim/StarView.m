da7da6d319ab8547438f535cfea61fe9114e5870
e-foley <efoley@calpoly.edu>
2019-06-09 16:36:03 -0700

    document starview

--- sim/StarView.m
+++ sim/StarView.m
@@ -1,9 +1,26 @@
+% Represents the covolution of stars with point spread functions for a
+% particular wavelength and a specific telescope diameter. Importantly, a
+% StarView object differs from a Psf object in that its dimensions are not
+% normalized to lambda/diameter: instead, they are in arcseconds.
+
 classdef StarView
     properties
-        data  % the color data: dims 1, 2, 3 are v, u, and RGB
-        pixels_per_as  % pixels/arcsecond
-        as_bounds  % limits of image in arcseconds [v1 v2; u1 u2]
-        diameter_in  % diameter of aperture in inches
-        wavelength_nm  % wavelength of light in nanometers
+        % A 2-D matrix of values proportional to the power density at known
+        % angular coordinates. First index is values of u spanning
+        % as_bounds(1,:); second index is values of v spanning as_bounds(2,:).
+        % Proportionality constant has no concise definition.
+        data
+        
+        % Spatial resolution of the data in pixels per arcsecond (pixels/as)
+        pixels_per_as
+        
+        % Angular bounds of data (arcseconds) [umin,umax;vmin,vmax]
+        as_bounds
+        
+        % Diameter of aperture "viewing" the StarView (inches)
+        diameter_in
+        
+        % Wavelength of light captured in this StarView (nanometers)
+        wavelength_nm
     end
 end
