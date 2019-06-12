5aaf4fe5b5443aea39012c017e085bf4698ac870
e-foley <efoley@calpoly.edu>
2019-06-11 18:24:28 -0700

    add extra apod note

--- sim/apertures/formApodization.m
+++ sim/apertures/formApodization.m
@@ -21,6 +21,7 @@
 function [M] = formApodization(canvas_size_px, rel_std_dev)
     M = zeros(canvas_size_px);
     center = (canvas_size_px + 1) / 2;
+    % Translucency decays as a Gaussian function of distance to image center.
     for x = 1:canvas_size_px
         for y = 1:canvas_size_px
             M(x,y) = exp(-(((x-center)^2 + (y-center)^2) / (2*(rel_std_dev*canvas_size_px)^2)));
